28be5a905ce09d63121c69bd1f050e1660316532
Catherine Karra and John Dow <catherine@>
2016-03-28 16:52:25 -0700

    finally split the sentence into words in an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,12 @@
 $(document).ready(function() {
   $("form#list").submit(function(event) {
-    var groceries = ["item1", "item2", "item3"];
+    var input = 'item'
+    var sentences = $("input#item").val();
+    var sentencesSplit = sentences.split(" ");
 
-    var mappy = groceries.map(function(grocery) {
-      return $('input#' + grocery).val();
+    var looptySatan = sentencesSplit.map(function(sentence) {
+      return sentence.length(loopySatan, 3);
     });
-    mappy.sort();
-    var doubleMappy = mappy.map(function(mapster) {
-      return mapster.toUpperCase();
-    });
-    $("#one").text(doubleMappy[0]);
-    $("#two").text(doubleMappy[1]);
-    $("#three").text(doubleMappy[2]);
-    $(".result").show();
-    event.preventDefault();
+    alert(looptySatan);
   });
 });
